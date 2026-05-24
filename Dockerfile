18d727028d1241e962a206157fb007f3b476b0fc
mi-tec <migara1225@gmail.com>
2026-05-24 19:43:18 +0100

    htmx server first commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Build stage
 FROM golang:1.26-alpine AS builder
 WORKDIR /app
-COPY go.mod ./
+COPY go.mod go.sum ./
 RUN go mod download
 COPY . .
 RUN CGO_ENABLED=0 GOOS=linux go build -o main ./cmd/main.go
@@ -10,4 +10,5 @@ RUN CGO_ENABLED=0 GOOS=linux go build -o main ./cmd/main.go
 FROM alpine:latest
 WORKDIR /root/
 COPY --from=builder /app/main .
+COPY --from=builder /app/views ./views
 CMD ["./main"]
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(c *echo.Context, w io.Writer, name string, data interface{}) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
 
 func main() {
-	fmt.Println("Hello from Docker!")
+	e := echo.New()
+
+	e.Use(middleware.RequestLogger()) // use the RequestLogger middleware with slog logger
+	e.Use(middleware.Recover())       // recover panics as errors for proper error handling
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.Render(200, "index", nil)
+	})
+
+	e.GET("/about-us", func(c *echo.Context) error {
+		return c.Render(200, "about-us.html", nil)
+	})
+
+	if err := e.Start(":1149"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
 }
